17819c56fda31544c843f53e99b6ff0d4ca7a24e
RiverAndBoat <842714673@qq.com>
2020-09-19 22:32:23 +0800

    p2s1

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LevelControllerTest {
+
+}
